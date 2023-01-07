7078cbaafa057acc29155989d13919684591db3b
Cody <cody@codyq.dev>
2023-01-06 18:19:00 -0600

    Word list & starting logic

--- server/src/actor.rs
+++ server/src/actor.rs
@@ -1,8 +1,18 @@
 use actix::{Actor, Context, Handler, Message};
+use itertools::Itertools;
+use once_cell::sync::Lazy;
 use rand::distributions::Alphanumeric;
 use rand::Rng;
 use uuid::Uuid;
 
+static DEFAULT_WORD_LIST: Lazy<Vec<String>> = Lazy::new(|| {
+    include_str!("assets/words.txt")
+        .split('\n')
+        .map_into()
+        .collect::<Vec<_>>()
+});
+
+#[derive(Debug, Clone, Eq, PartialEq)]
 enum PlayerRole {
     Admin,
     Member,
@@ -17,26 +27,40 @@ struct Player {
 }
 
 struct GameSettings {
-    word_list: Option<Vec<String>>,
-    time_to_guess_in_minutes: Option<u8>,
-    number_of_rounds: Option<u8>,
+    prompt_list: Vec<String>,
+    time_to_guess_in_seconds: u8,
+    number_of_rounds: u8,
+    allow_specators: bool,
+}
+
+impl Default for GameSettings {
+    fn default() -> Self {
+        Self {
+            prompt_list: DEFAULT_WORD_LIST.clone(),
+            time_to_guess_in_seconds: 30,
+            number_of_rounds: 10,
+            allow_specators: true,
+        }
+    }
 }
 
 struct OngoingGame {
-    current_word: String,
-    current_turn_index: i32,
+    current_prompt: String,
+    current_turn_index: i32, // index: not including spectators
+    current_round: i32,
 }
 
-enum Status {
-    Lobby(GameSettings),
+enum State {
+    Lobby,
     Playing(OngoingGame),
 }
 
 struct Game {
     id: Uuid,
-    status: Status,
+    state: State,
     room_code: String,
     players: Vec<Player>,
+    settings: GameSettings,
 }
 
 impl Game {
@@ -49,13 +73,10 @@ impl Game {
 
         Game {
             id: Uuid::new_v4(),
-            status: Status::Lobby(GameSettings {
-                word_list: None,
-                time_to_guess_in_minutes: None,
-                number_of_rounds: None,
-            }),
-            room_code,
+            state: State::Lobby,
             players: Vec::new(),
+            settings: GameSettings::default(),
+            room_code,
         }
     }
 }
@@ -67,6 +88,12 @@ impl Actor for Game {
 enum PlayerJoinResponse {
     Success,
     NicknameIsTaken,
+}
+
+enum StartGameResponse {
+    Started,
+    GameAlreadyStarted,
+    Unauthorized,
 }
 
 #[derive(Message)]
@@ -86,9 +113,9 @@ impl Handler<PlayerJoined> for Game {
         {
             Ok(PlayerJoinResponse::NicknameIsTaken)
         } else {
-            let player_role: PlayerRole = match self.status {
-                Status::Lobby(_) => PlayerRole::Member,
-                Status::Playing(_) => PlayerRole::Spectator,
+            let player_role: PlayerRole = match self.state {
+                State::Lobby => PlayerRole::Member,
+                State::Playing(_) => PlayerRole::Spectator,
             };
 
             self.players.push(Player {
@@ -97,7 +124,41 @@ impl Handler<PlayerJoined> for Game {
                 score: 0,
                 role: player_role,
             });
+
             Ok(PlayerJoinResponse::Success)
         }
+    }
+}
+
+#[derive(Message)]
+#[rtype(result = "Result<StartGameResponse, std::io::Error>")]
+struct StartGame {
+    initiator: Uuid,
+}
+
+impl Handler<StartGame> for Game {
+    type Result = Result<StartGameResponse, std::io::Error>;
+
+    fn handle(&mut self, msg: StartGame, _: &mut Context<Self>) -> Self::Result {
+        if self
+            .players
+            .iter()
+            .any(|it| it.id == msg.initiator && it.role == PlayerRole::Admin)
+        {
+            return match self.state {
+                State::Playing(_) => Ok(StartGameResponse::GameAlreadyStarted),
+                State::Lobby => {
+                    self.state = State::Playing(OngoingGame {
+                        current_prompt: self.settings.prompt_list.first().unwrap().clone(),
+                        current_turn_index: 0,
+                        current_round: 0,
+                    });
+
+                    Ok(StartGameResponse::Started)
+                }
+            };
+        }
+
+        Ok(StartGameResponse::Unauthorized)
     }
 }
